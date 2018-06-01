e9e5752bb1a2d1ff4a8fb517f64dc6fb3bcadd5d
dechenpema <dpema00@stuy.edu>
2018-06-01 13:56:55 -0400

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -1,1 +1,37 @@
+#!/usr/bin/python
+print "Content-type: text/html\n"
 
+import cgi
+
+#help you see errors
+import cgitb
+cgitb.enable()
+
+head = '''<!DOCTYPE html>
+<html>
+  <head>
+   <title>2017 NYC High School Directory</title>
+  </head>
+  <body>
+'''
+
+def convertToDictionary(fieldStorage):
+   output = {}
+   for key in fieldStorage.keys():
+     output[key] = fieldStorage[key].value
+   return output
+   
+form = convertToDictionary(cgi.FieldStorage()) 
+   
+fh = open("highschool.csv", "r")
+data = fh.readlines() #returns a list, each item is a line
+for row in data[1:]:
+  #removes the \n character and turns each row from a string to an array of values
+  row = row[:-1].split(",")
+  
+options = ""
+fruits = ["apple", "orange", "banana", "durian"]
+for fruit in fruits:
+  options += "<option value={fruit}>{fruit}</option>".format(fruit = fruit)
+
+dropdown = "<select>{options}</select>".format(options = options)
